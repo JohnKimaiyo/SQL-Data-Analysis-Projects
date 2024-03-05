733cc4953fe414a7453eaa11e2b76b6bdd0747c9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-03-05 16:04:37 +0300

    final

--- Addidas/adidas.sql
+++ Addidas/adidas.sql
@@ -6,13 +6,12 @@ SELECT MAX ([Total Sales]) AS Highest_Sales_amount
 SELECT AVG ([Total Sales]) AS Average_Sales_amount
   FROM [Adidas sales Report].[dbo].['Working Sheet$']
 
-
-
-
-
-
-
-
+/******Whats are the Top 5 Regions with the highest sales   ******/
+SELECT TOP 5[Region] ,sum([Total Sales]) As Total_Sales
+      
+  FROM [Adidas sales Report].[dbo].['Working Sheet$']
+  GROUP BY Region
+  ORDER BY Total_Sales DESC;
 
 
 
